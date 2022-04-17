2cd72880413b9614d277f09bd561f5053ee7ae6d
Maciovan Razvan <razvan.maciovan02@e-uvt.ro>
2022-04-17 15:56:03 +0300

    Sign in / Log in divider

--- HappinessDashboard/src/app/app.component.ts
+++ HappinessDashboard/src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: `
+    <input [(ngModel)]="username" #ctrl="ngModel" required>
+
+    <p>Value: {{ username }}</p>
+    <p>Valid: {{ ctrl.valid }}</p>
+
+    <button (click)="setValue()">Set value</button>
+  `,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ HappinessDashboard/src/app/user-form/user-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  username: string | undefined;
+  ShowName() {
+    console.log(this.username);
+  }
+
+}
