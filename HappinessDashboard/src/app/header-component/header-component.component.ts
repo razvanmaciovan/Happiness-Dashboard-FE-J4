f9e4507a9109290cd0d39d5dc9c4efce66ff5d97
Maciovan Razvan <razvan.maciovan02@e-uvt.ro>
2022-04-21 18:00:52 +0300

    Update header-component.component.ts
    Added commands to check if the header works, still needs routing

--- HappinessDashboard/src/app/header-component/header-component.component.ts
+++ HappinessDashboard/src/app/header-component/header-component.component.ts
@@ -12,21 +12,32 @@ export class HeaderComponentComponent implements OnInit {
   constructor() {
 
    }
-  items: MenuItem[] = [];
 
-    ngOnInit() {
+   items: MenuItem[] = [];
+   
+   CreateTeam() {
+       console.log("Create team");
+   }
+   ViewPoll() {
+       console.log("View polls");
+   }
+   SelectTeam() {
+         console.log("Select team");
+   }
+    ngOnInit(): void{
         this.items = [
             {
                 label: 'View polls',
                 icon: 'pi pi-fw pi-list',
-                url: 'http://localhost:4200/polls'
+                url: 'http://localhost:4200/polls',
+                command: (event) => this.ViewPoll()
             },
             {
                 label: 'Join a team',
                 icon: 'pi pi-fw pi-plus',
                 items: [
-                    {label: 'I already have a team', icon: 'pi pi-fw pi-users', url: 'http://localhost:4200/select'},
-                    {label: 'Create team', icon: 'pi pi-fw pi-user-plus', url: 'http://localhost:4200/create'},
+                    {label: 'I already have a team', icon: 'pi pi-fw pi-users', url: 'http://localhost:4200/select', command: (event) => this.SelectTeam()},
+                    {label: 'Create team', icon: 'pi pi-fw pi-user-plus', url: 'http://localhost:4200/create', command: (event) => this.CreateTeam() },
                 ]
             }
         ];
