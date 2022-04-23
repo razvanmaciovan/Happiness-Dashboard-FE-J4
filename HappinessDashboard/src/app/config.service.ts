f51610a8c11a594fb098bbc69a06fdff4893c443
Maciovan Razvan <razvan.maciovan02@e-uvt.ro>
2022-04-23 18:53:55 +0300

    CRUD update
    Achieved reading of the user list

--- HappinessDashboard/src/app/config.service.ts
+++ HappinessDashboard/src/app/config.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable, tap } from 'rxjs';
+import { IUser } from './user-form/user-form.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-  readonly configUrl = 'localhost:8080/';
+  readonly configUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
+  getUsersList():Observable<IUser[]> {
+    return this.http.get<IUser[]>(this.configUrl + "/users");
+  }
+  getUser(username:string):Observable<IUser> {
+    return this.http.get<IUser>(this.configUrl + "/users/" + username);
+  }
 }
--- HappinessDashboard/src/app/user-form/user-form.component.ts
+++ HappinessDashboard/src/app/user-form/user-form.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { ConfigService } from '../config.service';
 
+export interface IUser {
+  userId: number;
+  userName: string;
+  userPassword: string;
+}
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
@@ -7,15 +15,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  username: string | undefined;
+  userList$!: Observable<IUser[]>;
+  currentUser$!:Observable<IUser>;
+  username!: string;
   password: string | undefined;
-  ShowName() {
-    console.log(this.username);
-    console.log(this.password);
+  
+  constructor(private service:ConfigService) { }
+
+  Login(): void {
+    this.currentUser$ = this.service.getUser(this.username)
+    console.log(this.currentUser$);
+    
   }
+  ngOnInit(): void {
+    this.userList$ = this.service.getUsersList();
+    this.userList$.forEach(element => {
+      console.log(element);
+      
+    });
+    
+  }
+
 
 }
